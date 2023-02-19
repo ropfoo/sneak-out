66918bf95501d71408251097ac5e27f61f99d49d
RopFoo <ropfoo@gmail.com>
2023-02-19 12:07:26 +0100

    added user profile image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import SearchMoviesBar from "./Search/SearchMoviesBar";
+import Image from "next/image";
+import defaultProfileImage from "../public/assets/profile_default.png";
 
 export default function Header() {
   const { pathname } = useRouter();
   const isSearchMoviesPage = pathname === "/";
 
+  const { data: session } = useSession();
+
+  console.log(session);
   return (
     <header className="sticky top-0 flex w-full flex-col bg-slate-900 p-4 text-slate-300">
-      <p>hello party people!</p>
-      <Link href={"/profile"}>
-        <p>profile</p>
-      </Link>
-      <Link href={"/"}>
-        <p>start</p>
-      </Link>
+      <div className="mb-4 flex items-center justify-between">
+        <Link href={"/"}>
+          <p>search</p>
+        </Link>
+        <Link href={"/profile"}>
+          <div className="w-12">
+            <Image
+              src={session?.user?.image ?? defaultProfileImage}
+              width={48}
+              height={48}
+              alt="profile image"
+            />
+          </div>
+        </Link>
+      </div>
       {isSearchMoviesPage && <SearchMoviesBar />}
     </header>
   );
 }
+// session?.user?.image ??
