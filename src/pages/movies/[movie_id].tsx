bd78d5babe2eccca93f02b882061f4b3067636e4
RopFoo <ropfoo@gmail.com>
2023-02-21 00:02:28 +0100

    setup basic time interval form

--- src/pages/movies/[movie_id].tsx
+++ src/pages/movies/[movie_id].tsx
@@ -1,14 +1,58 @@
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
+import { useForm } from "react-hook-form";
 
 export default function MoviePage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<{ startTime: number; endTime: number }>();
+
   const { query } = useRouter();
   const movieId = query.movie_id?.toString() ?? "";
-  const { data } = trpc.movies.get.useQuery({ id: movieId });
-  console.log(data);
+  const { data: movie } = trpc.movies.get.useQuery({ id: movieId });
+  const { data: session } = useSession();
+  const isLoggedIn = !!session?.user;
+  // console.log("Movie:", movie);
+
+  const onSubmit = (data: { startTime: number; endTime: number }) => {
+    console.log(data);
+  };
+
   return (
-    <main>
-      <h1>{data?.title}</h1>
+    <main className="h-screen">
+      <div
+        style={{
+          backgroundImage: `url(https://www.themoviedb.org/t/p/w1920_and_h800_bestv2/${movie?.poster_path}), url(https://www.themoviedb.org/t/p/w533_and_h300_bestv2/${movie?.poster_path})`,
+        }}
+        className={`h-36 w-full bg-cover bg-center bg-no-repeat`}
+      ></div>
+
+      <h1>{movie?.title}</h1>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="text-dark"
+            placeholder="start"
+            type={"number"}
+            {...register("startTime", { required: true })}
+          />
+          {errors.startTime && <span>This field is required</span>}
+
+          <input
+            className="text-dark"
+            placeholder="end"
+            {...register("endTime", { required: true })}
+          />
+          {errors.endTime && <span>This field is required</span>}
+
+          <input type={"submit"} />
+        </form>
+      </div>
     </main>
   );
 }
